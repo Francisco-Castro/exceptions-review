7c5e7d1d9ddb9276dcb4258ffcf1396a1072f559
Francisco Castro <catra@Franciscos-MBP.fios-router.home>
2019-05-27 22:02:32 -0400

    Exceptions used: StringIndexOutOfBoundsException, NullPointerException, Exception. Try and multiples atch were used in order to handle exceptions. Operator or | was used to combine multiples exceptions in only one catch block.

--- src/main/java/MainClass0.java
+++ src/main/java/MainClass0.java
@@ -1,6 +1,32 @@
 public class MainClass0 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        try {
+            String str = "A char string";
+            System.out.println("Before problem.");
+            char ch = str.charAt(20);
+            System.out.println("After problem.");
+        }
+        catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Hey - your string's not that big");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Oops, perhaps you tried to access a null string!");
+        }
+        catch (Exception e) {
+            System.out.println("Oops, an unexpected problem occurred!");
+        }
+        System.out.println("Now what?");
+
+        try {
+            String str = "Another char string";
+            System.out.println("Before problem.");
+            char ch = str.charAt(20);
+            System.out.println("After problem.");
+        }
+        catch ( StringIndexOutOfBoundsException | NullPointerException e) {
+            System.out.println("Oops - You got string problems buddy!");
+        }
     }
 }
